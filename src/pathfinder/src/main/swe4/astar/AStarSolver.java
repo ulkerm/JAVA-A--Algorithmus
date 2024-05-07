2702255b75b3ea3caa3aaab057e6da5572d88253
Ulkar Mikayilova <ulkersamedova@live.com>
2024-05-07 12:06:43 +0200

    initial commit

--- src/pathfinder/src/main/swe4/astar/AStarSolver.java
+++ src/pathfinder/src/main/swe4/astar/AStarSolver.java
@@ -1,14 +1,56 @@
 package swe4.astar;
 
-import java.util.List;
+import java.util.*;
 
 public class AStarSolver {
+
+  private final Set<SearchNode> nodes = new HashSet<>();
+  private final Map<State, Set<SearchNode>> connections = new HashMap<>();
+  private PriorityQueue<SearchNode> openList = new PriorityQueue<>();
+
+  private double getNodeCost(SearchNode node) {
+    for (SearchNode n : nodes) {
+      if (n.equals(node)) {
+        return n.costsFromStart();
+      }
+    }
+    return Double.POSITIVE_INFINITY;
+  }
+
+  private void generateSuccessorNodes(SearchNode current, State target) {
+    List<? extends Transition> possibleTransitions = current.getState().transitions();
+    for (Transition transition : possibleTransitions) {
+      State nextState = current.getState().apply(transition);
+      SearchNode succ = new SearchNode(nextState, target, current, transition);
+      succ.setCostsFromStart(current.costsFromStart() + transition.costs());
+      if (connections.containsKey(succ.getState()) || succ.costsFromStart() < getNodeCost(succ)) {
+        openList.add(succ);
+      }
+    }
+  }
 
   // Berechnet eine kostenoptimale Folge von Transitionen, welche den gegebenen
   // Anfangszustand initialState in den Zielzustand targetState überführt.
   // Wirft NoSolutionException (Checked Exception), falls es keine derartige
   // Folge von Transitionen gibt.
   public List<? extends Transition> solve(State initialState, State targetState) throws NoSolutionException {
-    throw new UnsupportedOperationException();
+    SearchNode n = new SearchNode(initialState, targetState);
+    n.setCostsFromStart(0);
+    openList.add(n);
+
+    if (!connections.containsKey(initialState)) {
+      connections.put(initialState, new HashSet<>());
+    }
+    connections.get(initialState).add(n);
+
+    while (!openList.isEmpty()) {
+      SearchNode currentNode = openList.poll();
+      if (currentNode.getState().equals(targetState)) {
+        return currentNode.getTransitionsFromStart();
+      }
+      generateSuccessorNodes(currentNode, targetState);
+    }
+
+    throw new NoSolutionException();
   }
 }
--- src/pathfinder/src/main/swe4/astar/SearchNode.java
+++ src/pathfinder/src/main/swe4/astar/SearchNode.java
@@ -1,5 +1,6 @@
 package swe4.astar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // SearchNode ist eine Hilfsklasse, die zur Implementierung des A*-Algorithmus 
@@ -9,56 +10,87 @@ import java.util.List;
 // kann auch eine Schätzung für den Weg zum Zielknoten berechnen.
 public class SearchNode implements Comparable<SearchNode> {
 
+  private State state;
+  private State targetState;
+  private Transition transition;
+  private SearchNode predecessor;
+  private double costsFromStart;
+
   // Suchknoten mit dem Zustand state und dem Zielzustand target initialisieren.
   // Es existiert kein Vorgängerknoten.
-  public SearchNode(State state, State targetState) { throw new UnsupportedOperationException(); }
+  public SearchNode(State state, State targetState) {
+    this.state = state;
+    this.targetState = targetState;
+    this.predecessor = null;
+    this.transition = null;
+    this.costsFromStart = 0;
+  }
 
   // Suchknoten mit dem Zustand state und dem Zielzustand target initialisieren.
   // predecessor ist der Vorgängerknoten, von dessen Zustand man mit transition
   // zum Zustand dieses Knotens kommt.
-  public SearchNode(State state, State targetState, SearchNode predecessor, Transition transition) { throw new UnsupportedOperationException(); }
+  public SearchNode(State state, State targetState, SearchNode predecessor, Transition transition) {
+    this(state, targetState);
+    this.transition = transition;
+    this.predecessor = predecessor;
+    this.costsFromStart = 0;
+    SearchNode n = this.predecessor;
+    while (n != null) {
+      this.costsFromStart += n.costsFromStart;
+      n = n.predecessor;
+    }
+
+    if (this.transition != null) {
+      this.costsFromStart += transition.costs();
+    }
+  }
 
   // Gibt den Zustand dieses Knotens zurück.
-  public State getState() { throw new UnsupportedOperationException(); }
+  public State getState() {
+    return state;
+  }
 
   // Gibt den Vorgängerknoten zurück.
-  public SearchNode getPredecessor() { throw new UnsupportedOperationException(); }
+  public SearchNode getPredecessor() {
+    return predecessor;
+  }
 
   // Setzt den Vorgängerknoten.
   public SearchNode setPredecessor(SearchNode predecessor) {
-    throw new UnsupportedOperationException();
+    this.predecessor = predecessor;
+    return predecessor;
   }
 
   // Gibt Transition zurück, die Vorgängerknoten in diesen Knoten überführt.
   public Transition getTransition() {
-    throw new UnsupportedOperationException();
+    return transition;
   }
 
   // Setzt Transition, die Vorgängerknoten in diesen Knoten überführt.
   public void setTransition(Transition transition) {
-    throw new UnsupportedOperationException();
+    this.transition = transition;
   }
 
   // Gibt Kosten vom Startknoten bis zu diesem Knoten zurück.
   public double costsFromStart() {
-    throw new UnsupportedOperationException();
+    return costsFromStart;
   }
 
   // Setzt die Kosten vom Startknoten bis zu diesem Knoten.
   public void setCostsFromStart(double costsFromStart) {
-    throw new UnsupportedOperationException();
+    this.costsFromStart = costsFromStart;
   }
 
   // Gibt geschätzte Kosten von diesem Knoten bis Zum Zielknoten zurück.
   public double estimatedCostsToTarget() {
-    throw new UnsupportedOperationException();
-  }
+    return this.state.estimatedCostsToTarget(this.targetState);
+    }
 
   // Gibt Schätzung der Kosten vom Startknoten über diesen Knoten bis zum
   // Zielknoten zurück (= Kosten bis zu diesem Knoten + geschätzte Kosten
   // bis zum Zielknoten).
   public double estimatedTotalCosts() {
-    throw new UnsupportedOperationException();
+    return this.costsFromStart + this.estimatedCostsToTarget();
   }
 
   // Gibt zurück, ob dieser Knoten und der Knoten other denselben Zustand
@@ -67,13 +99,16 @@ public class SearchNode implements Comparable<SearchNode> {
   // Muss konsistent mit compareTo implementiert werden:
   // a.equals(b) <==> a.comparesTo(b) == 0.
   @Override
-  public boolean equals(Object obj) {throw new UnsupportedOperationException();}
+  public boolean equals(Object obj) {
+    SearchNode other = (SearchNode) obj;
+    return (this.state.equals(other.getState()) && this.targetState.equals(other.targetState));
+    }
 
   // Notwendig für Verwendung in Hash-basierten Behältern. Muss konsistent
   // mit equals implementiert werden: a.equals(b) => a.hashCode() == b.hashCode.
   @Override
   public int hashCode() {
-    throw new UnsupportedOperationException();
+    return state.hashCode() + targetState.hashCode();
   }
 
   // Vergleicht zwei Knoten auf Basis der gesch�tzten Gesamtkosten.
@@ -81,12 +116,23 @@ public class SearchNode implements Comparable<SearchNode> {
   //  0: Dieser Knoten und other sind gleich.
   // >0: Kosten dieses Knotens sind höher als Kosten von other.
   @Override
-  public int compareTo(SearchNode other) {throw new UnsupportedOperationException();}
+  public int compareTo(SearchNode other) {
+    return (this.costsFromStart > other.costsFromStart) ? 1 :
+            ((this.costsFromStart < other.costsFromStart) ? -1 : 0);
+  }
 
   // Konvertiert die Knotenliste, die bei diesem Knoten ihren Ausgang hat,
   // in eine Liste von Transitionen. Da der Weg in umgekehrter Reihenfolge
   // gespeichert ist, muss die Transitionsliste invertiert werden.
-  public List<? extends Transition> getTransitionsFromStart() { throw new UnsupportedOperationException(); }
+  public List<? extends Transition> getTransitionsFromStart() {
+    List<Transition> transitions = new ArrayList<Transition>();
+    SearchNode other = this;
+    while (other.predecessor != null) {
+      transitions.add(other.getTransition());
+      other = other.predecessor;
+    }
+    return transitions.reversed();
+  }
 
   @Override
   public String toString() { throw new UnsupportedOperationException(); }
